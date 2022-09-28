c96d67e2d96430cf497400ef57daea5788048312
M-Lef <melvillelefebvre@gmail.com>
2022-09-28 21:26:36 +0200

    STEP 7

--- game/graphic.js
+++ game/graphic.js
@@ -24,10 +24,12 @@ function init()
 
     noGround = [];
     ground = new Ground(0xffffff, WIDTH, HEIGHT, 10);
-    //scene.add(ground.graphic)
 
     player1 = new Player("player1", 0xffff00, new THREE.Vector2(50, 0), 0);
     scene.add(player1.graphic);
+
+    ennemy = new Ennemy("ennemy", 0xff0000, new THREE.Vector2(0, 0), Math.PI);
+    scene.add(ennemy.graphic);
 
     light1 = new Light("sun", 0xffffff, "0,0,340");
     scene.add(light1);
